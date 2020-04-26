d91ffb78a833c3ea696bb47e3534911171168ef3
Nguyen Dang Huy <huy.d.nguyen@eastagile.com>
2020-04-26 14:28:09 +0700

    fix controller

--- src/resources/data/data.controller.js
+++ src/resources/data/data.controller.js
@@ -1,51 +1,37 @@
 import db from '../../models';
+import { Op } from 'sequelize';
 
 const Data = db.Data;
 
-export const getCurrentRecord = async (request, respond) => 
-{
-	try
-	{
-		Data.findAll ( { limit: 1, order: [ [ 'createdAt', 'DESC' ] ], } )
-			.then (function (entries)
-				{
-					var result = 
-						{
-							temperature: entries[0].temperature,
-							humid: entries[0].humid,
-							light: entries[0].light,
-							createdAt: entries[0].createdAt,
-							updatedAt: entries[0].updatedAt,
-						};
-
-					respond.status (200).json (result);
-				});
-	}
-	catch (error)
-	{
-		return request.status(400).json({ error: error.message });
-	}
+export const getCurrentRecord = async (request, respond) => {
+  try {
+    const entries = await Data.findAll({
+      limit: 1,
+      order: [['createdAt', 'DESC']],
+    });
+    let result = {
+      temperature: entries[0].temperature,
+      humid: entries[0].humid,
+      light: entries[0].light,
+      createdAt: entries[0].createdAt,
+      updatedAt: entries[0].updatedAt,
+    };
+    return respond.status(200).json({ data: result });
+  } catch (error) {
+    return request.status(400).json({ error: error.message });
+  }
 };
 
-export const getRecordInRange = async (request, respond) => 
-{
-	try
-	{
-		var start = request.query.start;
-		var end = request.query.end;
-		Data.findAll( { order: [ [ 'createdAt', 'ASC' ] ], where: { createdAt: { [Op.gte]: start, [Op.lte]: end, } }, } )
-			.then( function (entries)
-				{
-					var result = 
-						{
-							records: entries,
-						};
-
-					respond.status (200).json (result);
-				});
-	}
-	catch (error)
-	{
-		return request.status(400).json({ error: error.message });
-	}
+export const getRecordInRange = async (request, respond) => {
+  try {
+    let start = request.query.start;
+    let end = request.query.end;
+    const entries = await Data.findAll({
+      order: [['createdAt', 'ASC']],
+      where: { createdAt: { [Op.gte]: start, [Op.lte]: end } },
+    });
+    return respond.status(200).json({ data: entries });
+  } catch (error) {
+    return request.status(400).json({ error: error.message });
+  }
 };
