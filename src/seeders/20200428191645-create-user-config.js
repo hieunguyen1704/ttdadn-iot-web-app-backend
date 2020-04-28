fabdb72462ce2f0fbff7554fb660d6f194352551
hieupropy1999 <hoanghieupro1999@gmail.com>
2020-04-29 03:05:40 +0700

    create seeder file

---
+++ src/seeders/20200428191645-create-user-config.js
@@ -0,0 +1,13 @@
+'use strict';
+const factory = require('../factory/user-config.factory');
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  up: (queryInterface, Sequelize) => {
+    return factory.createMany('user-config', 10, {}, {});
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('UserConfigs', null, {});
+  }
+};
