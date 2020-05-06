951a2e9dd1515a05b4ba131166c249fe931a97a2
hoitran <tranxuanhoi199@gmail.com>
2020-05-06 15:54:28 +0700

    done backend

--- src/resources/user/auth.router.js
+++ src/resources/user/auth.router.js
@@ -11,9 +11,9 @@ router
 router.get('/', auth, async (req, res) => {
     try {
         // const user = await User.findById(req.user.id).select('-password')
-        console.log(req.user)
+        // console.log(req.user)
         const user = await User.findOne({ where: { id: req.user.id } })
-        res.json(user)
+        res.json({ username: user.username, id: user.id })
     } catch (err) {
         console.log((err.message));
         res.status(500).send('Server error');
