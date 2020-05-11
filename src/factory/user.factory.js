b0512017b7ee2284139a8f2a51429675a858a2f0
hoitran <tranxuanhoi199@gmail.com>
2020-05-11 10:22:15 +0700

    user factory

--- src/factory/user.factory.js
+++ src/factory/user.factory.js
@@ -4,9 +4,9 @@ const db = require('../models');
 
 factory.define('data', db.Data, () => {
     let attrs = {
-        temperature: factory.chance('floating', { min: 0, max: 100 }),
-        humid: factory.chance('floating', { min: 0, max: 100 }),
-        light: factory.chance('natural', { min: 0, max: 1023 })
+        username: factory.chance('floating', { min: 0, max: 100 }),
+        email: factory.chance('floating', { min: 0, max: 100 }),
+        password: factory.chance('natural', { min: 0, max: 1023 })
     };
     return attrs;
 });
