e38709fc959db295cf45015b3b5ef0bf10634c9e
hieupropy1999 <hoanghieupro1999@gmail.com>
2020-05-17 01:52:22 +0700

    add publish controller

--- src/resources/publish_rule/publish.controller.js
+++ src/resources/publish_rule/publish.controller.js
@@ -1,15 +1,16 @@
 import {publish} from '../../service/client/publish';
-export const PublishData = (req, res) => {
+import db from '../../models';
+export const PublishData = async (req, res) => {
     try {
-    //   const isAuto = req.user.isAuto;
-        const isAdmin = req.user.isAdmin;
-        if(isAdmin){
+        const user = await db.User.findOne({ where: { id: req.user.id } });
+        const {isAdmin,isAuto} = user;
+        if(isAdmin && isAuto){
             const message = publish("Hello World"); 
             return res.status(200).json({ data: message});
-          }else{
+        }
+        else{
             return res.status(200).json({ data: "publish denied"});
-          }
-
+        }
     } catch (error) {
       console.log(error.message);
       return res.status(400).json({ error: error.message });
