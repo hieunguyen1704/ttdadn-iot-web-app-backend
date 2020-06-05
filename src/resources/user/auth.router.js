77014a5611ec1d6dc7c56e50897fd9a5be9296b6
hoitran <tranxuanhoi199@gmail.com>
2020-06-05 10:24:54 +0700

    add api for auto

--- src/resources/user/auth.router.js
+++ src/resources/user/auth.router.js
@@ -3,7 +3,7 @@ import { requestLogin, getUser } from './auth.controller';
 import auth from './middleware/auth'
 const router = Router();
 import db from '../../models';
-
+import authAdmin from './middleware/admin';
 const User = db.User;
 router
     .route('/')
@@ -19,6 +19,27 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server error');
     }
 }
+)
+
+router.post('/configAuto', authAdmin, async (req, res) => {
+    try {
+        const userID = req.user.id;
+        const user = await User.findOne({ where: { id: userID } });
+        const isAuto = user.isAuto;
+        if (isAuto == true) {
+            user.isAuto = false;
+        }
+        else {
+            user.isAuto = true;
+        }
+        await user.save();
+        res.status(200).json({ username: user.username, id: user.id, isAuto: user.isAuto, isAdmin: user.isAdmin })
+    } catch (err) {
+        console.log(err.message);
+        res.status(400).json({ msg: 'Server error' })
+    }
+}
+
 )
 
 export default router;
